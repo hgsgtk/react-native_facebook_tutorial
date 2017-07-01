778759e804a9ae326f7df7b7c5e2568da132965f
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-07-01 16:22:10 +0900

    FexDimensionBasics

--- index.ios.js
+++ index.ios.js
@@ -13,16 +13,18 @@ import {
     StyleSheet
 } from 'react-native';
 
-class FixedDimensionBasics extends Component{
+class FlexDimensionBasics extends Component{
   render(){
     return(
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1}}>
+        <View style={{height: 300 , backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'gray'}} />
+        <View style={{flex: 3, backgroundColor: 'orange'}} />
+        <View style={{flex: 4, backgroundColor: 'red'}} />
+
       </View>
-      )
+    );
   }
 }
 
-AppRegistry.registerComponent('rntutorial', () => FixedDimensionBasics);
+AppRegistry.registerComponent('rntutorial', () => FlexDimensionBasics);
